7d704e24e3aa2b6e8ad4915ef0b16b34b246ec5a
ajp4707 <52212289+ajp4707@users.noreply.github.com>
2021-08-06 11:29:11 -0400

    Edit configs

--- Pong/Pong/src/config.py
+++ Pong/Pong/src/config.py
@@ -44,14 +44,14 @@ TEXTPAUSELENGTH = 3
 SIZE = (SCREENW, SCREENH)
 
 
-## Debug Constants for ease of access
-## Comment out below when running for real
-WINSCORE = 10
-SWITCHROUNDS = 2    # switch serves after how many rounds
-TIMELIMIT = False   
-MAXTIME = 185   # Only applied when TIMELIMIT = true
+### Debug Constants for ease of access
+### Comment out below when running for real
+#WINSCORE = 10
+#SWITCHROUNDS = 2    # switch serves after how many rounds
+#TIMELIMIT = False   
+#MAXTIME = 185   # Only applied when TIMELIMIT = true
 
-# Minor adjustments
-PAUSELENGTH = 1.5   # Seconds to pause after score, and before serve
-COUNTFROM = 0      # Seconds of countdown before starting game
-TEXTPAUSELENGTH = 0.5
+## Minor adjustments
+#PAUSELENGTH = 1.5   # Seconds to pause after score, and before serve
+#COUNTFROM = 0      # Seconds of countdown before starting game
+#TEXTPAUSELENGTH = 0.5
